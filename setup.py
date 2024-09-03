4b16acd156a39f06710b3acde623c7ec3e94703b
sumitkudale21 <sumitkudale3545@gmail.com>
2024-09-03 12:55:59 +0530

    template added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'sumit kudale',
+    author_email= 'sumitkudale3545@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
