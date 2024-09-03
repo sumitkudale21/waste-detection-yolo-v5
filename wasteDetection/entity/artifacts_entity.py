9bb55ada25a393725cca43c6de66bb645857fb41
sumitkudale21 <sumitkudale3545@gmail.com>
2024-09-03 15:49:53 +0530

    data validation added

--- wasteDetection/entity/artifacts_entity.py
+++ wasteDetection/entity/artifacts_entity.py
@@ -7,9 +7,9 @@ class DataIngestionArtifact:
 
 
 
-# @dataclass
-# class DataValidationArtifact:
-#     validation_status: bool
+@dataclass
+class DataValidationArtifact:
+    validation_status: bool
 
 
 
